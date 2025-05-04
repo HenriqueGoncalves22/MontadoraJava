08c897da027237559a76a78e37d80c3dea9a53f2
HenriqueGoncalves22 <Henrique.gonc.gamer@gmail.com>
2025-05-04 15:21:24 -0300

    Correções de sintaxe

--- src/br/com/montadora/teste/MotoSelecionar.java
+++ src/br/com/montadora/teste/MotoSelecionar.java
@@ -1,9 +1,28 @@
 package br.com.montadora.teste;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import br.com.montadora.conexao.Conexao;
+import br.com.montadora.dao.MotoDAO;
+import br.com.montadora.model.Moto;
+
 public class MotoSelecionar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Connection con = Conexao.abrirConexao();
+        MotoDAO motodao = new MotoDAO(con);
+		
+		ArrayList<Moto> lista = motodao.retornarDados();
+		
+		if(lista != null) {
+			for(Moto moto: lista) {
+				System.out.println("Montadora: " + moto.getMontadora() + " | " +
+			      "Nome: " + moto.getNome() + " | " + "Quantidade de Adesivos: " + 
+			      moto.getQuantidadeAdesivos() );
+			}
+		}
+			
 
 	}
 
